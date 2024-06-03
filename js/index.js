68e14a14672b3eff559c28589145c10260ddce05
nathan3453 <93751466+nathan3453@users.noreply.github.com>
2024-06-03 12:51:33 +1200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
 function message_send() {
     const message = document.getElementById("message");
     const message_value = message.value;
@@ -6,19 +12,27 @@ function message_send() {
     const name_value = name.value;
     console.log(name_value);
     console.log(message_value);
-    console.log(`${name_value}| ${message_value}`)
+    console.log(`${name_value}| ${message_value}`);
+    const button = document.getElementById("button");
 
     fetch("https://discord.com/api/webhooks/1232461159999995964/0SWbhzcYfjMawl5kYl-6IDQiIDTqjDyH7kPwYoWdfcBuzeNwlT7WGQOtvMpzptQB6HnW", {
         
         method: "POST",
         body: JSON.stringify({
+          username:
+            name_value,
           content:
-            `${name_value} ${message_value}`
+            message_value
           
         }),
         headers: {
           "Content-type": "application/json; charset=UTF-8"
         }
     });
+    button.innerHTML = "Sent!";
+    sleep(1500).then(() => {
+      button.innerHTML = "Send"
+  });
+
 }
     
